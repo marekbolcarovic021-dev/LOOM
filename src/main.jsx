67c5c7b7b5d57ca0e5f1f58e4efddff98b1f7c2d
marekbolcarovic021-dev <marekbolcarovic021@gmail.com>
2026-08-29 21:12:16 +0200

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
-
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import "./locales/i18n";
 
@@ -18,6 +18,7 @@ createRoot(document.getElementById("root")).render(
       <FinanceProvider>
         <CurrencyProvider>
           <App />
+           <Analytics />
 
           <Toaster
             position="top-right"
